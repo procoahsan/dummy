6a3592b6e15d1b3aafb59626cd018f94e01cd326
procoahsan <82664418+procoahsan@users.noreply.github.com>
2023-08-19 22:35:30 +0500

    db connection and middleware

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express()
+
+const PORT = process.env.PORT || 5000
+const Connection = "mongodb+srv://prcoahsan:hello123@cluster0.vdyufty.mongodb.net/?retryWrites=true&w=majority"
+
+mongoose.connect(Connection , {useNewUrlParser: true, useUnifiedTopology: true}).then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(express.json())
+app.use(cors());
